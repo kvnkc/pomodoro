172e32aca14f52ecb740217855f30692334f4328
Cheung <cheung@tdfcu.com>
2022-10-26 15:05:26 -0400

    Added countdown function and set timer function for pomodoro

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from cgitb import reset
 import tkinter
-from turtle import bgcolor
+import math
 
 PINK = '#e2979c'
 RED = '#e7305b'
@@ -10,7 +9,28 @@ FONT_NAME = 'Courier'
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+# Pomodoro Timer
 
+
+def start_timer():
+    count_down(5*60)
+
+# Pomodoro Countdown
+
+
+def count_down(count):
+    count_min = math.floor(count/60)  # gives the minute
+    count_sec = count % 60  # gives remainder number of seconds
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+# Pomodoro UI
 window = tkinter.Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -26,11 +46,12 @@ check_mark_label.grid(row=3, column=1)
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = tkinter.PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text='00:00', fill='white',
-                   font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white',
+                                font=(FONT_NAME, 35, 'bold'))
 canvas.grid(row=1, column=1)
 
-start_button = tkinter.Button(text="Start")
+
+start_button = tkinter.Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = tkinter.Button(text='Reset')
