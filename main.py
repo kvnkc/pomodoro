ecb2d8e51cc0aa6422e72e02d2de5d258462cdef
Cheung <cheung@tdfcu.com>
2022-10-26 12:27:48 -0400

    Added buttons and labels

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 import tkinter
 from turtle import bgcolor
 
@@ -14,11 +15,25 @@ window = tkinter.Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = tkinter.Label(
+    text='Timer', font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW, highlightthickness=0)
+timer_label.grid(row=0, column=1)
+
+check_mark_label = tkinter.Label(
+    text='✓', font=(FONT_NAME, 14, 'bold'), fg=GREEN, bg=YELLOW, highlightthickness=0)
+check_mark_label.grid(row=3, column=1)
+
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = tkinter.PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato)
 canvas.create_text(100, 130, text='00:00', fill='white',
                    font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_button = tkinter.Button(text="Start")
+start_button.grid(row=2, column=0)
+
+reset_button = tkinter.Button(text='Reset')
+reset_button.grid(row=2, column=2)
 
 window.mainloop()
